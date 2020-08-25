58d764061755664a6e25e88ba1f2d45f09f2acd1
yerx <yer.x.xiong@gmail.com>
2020-08-25 08:54:50 -0400

    completed exercise

---
+++ src/Debug.java
@@ -0,0 +1,15 @@
+public class Debug {
+    public static void main(String[] args) {
+        int number1=10;
+        int number2=5;
+        int number3=15;
+        int product;
+        double sum;
+        String Product="Product ";
+        String Sum="Sum ";
+
+        product=number1*number2*number3;
+        sum=number1+number2+number3;
+        System.out.println(Product+product+"\n"+Sum+sum);
+    }
+}
